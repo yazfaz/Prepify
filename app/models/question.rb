e2a2c93000b2be67cd915af20427aee24936661d
veleary <veleary@gmail.com>
2013-10-22 18:03:48 -0500

    trying some stuff for serialized array

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,11 @@
 class Question < ActiveRecord::Base
   serialize :choices, Array
+  attr_accessible :choices
   has_one :page, as: :pageable
 
   has_one :answer
+
+  def serialize_choices
+    self.choices = self.choices.split(',').collect()
+  end
 end
