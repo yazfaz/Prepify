c9b26521f10f7add48f2dbf192a8d141ea687e6f
Joe Marshall <joseph.charles.marshall@gmail.com>
2013-10-22 22:19:20 -0500

    Updates app_controller so that admin name is saved in DB

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Accept the following parameters if in the devise controller
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # Adds values that can be stored in the DB from user input
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
 end
