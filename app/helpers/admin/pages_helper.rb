966271b8ca7cb73c8e7f3088649dda2f7095dd3d
MakerSquare Student <students@themakersquare.com>
2013-10-23 16:22:08 -0500

    Logic moved from Page show view to helper

--- app/helpers/admin/pages_helper.rb
+++ app/helpers/admin/pages_helper.rb
@@ -1,5 +1,13 @@
 module Admin::PagesHelper
 
+  def index_content(page)
+    if page.pageable 
+      "<td><%= page.pageable.body %></td>".html_safe
+    else
+      "<td><%= "Please add content" %></td>".html_safe
+    end
+  end
+
   def show_body(pageable = @pageable)
     if pageable.body.empty?
       "<p> No content </p>".html_safe 
@@ -7,4 +15,21 @@ module Admin::PagesHelper
        pageable.body
     end
   end
+
+  def show_choices(pageable = @pageable)
+    if pageable.choices.empty?
+      "<p> No content </p>".html_safe 
+    else  
+       pageable.choices
+    end
+  end
+
+  def show_correct_answer(pageable = @pageable)
+    if pageable.correct_answer.empty?
+      "<p> No content </p>".html_safe 
+    else  
+       pageable.correct_answer
+    end
+  end
+
 end
