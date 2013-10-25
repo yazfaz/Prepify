daacd96c222fffc825adaf7c0fd3c6553b0a4973
Joe Marshall <joseph.charles.marshall@gmail.com>
2013-10-25 13:56:43 -0500

    Fixes mail registration bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     super
+    if @user.save
     Notifier.subscription_confirmation(@user).deliver
+    else
+    end
   end
 
 end
