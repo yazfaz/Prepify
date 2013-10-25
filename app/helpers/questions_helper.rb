4f536f6063eefbe5f4ed8cb01387c06a8f8a55cc
veleary <veleary@gmail.com>
2013-10-25 11:35:45 -0500

    check answer button isand logic is working

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,10 +1,3 @@
 module QuestionsHelper
 
-  def check_answer(question)
-     question.choices.each do |choice|
-      if choice = question.correct_answer
-        return choice
-      end
-    end
   end
-end
