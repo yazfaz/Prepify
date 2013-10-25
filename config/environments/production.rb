af73e53048c99526fb7a4b23856f7781618cb283
veleary <veleary@gmail.com>
2013-10-25 16:44:49 -0500

    update production environment with mailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,13 @@ Prepify::Application.configure do
 
   # Setup production environment mailer domain
   config.action_mailer.default_url_options = { :host => 'www.prepify.herokuapp.com' }
-
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV["EMAIL_ACCOUNT"],
+    :password             => ENV["EMAIL_PASSWORD"],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
