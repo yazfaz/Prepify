e40599ec29e8a231dbe2c5d6fa4ff57453a832cd
veleary <veleary@gmail.com>
2013-10-27 18:41:56 -0500

    fix bug on progress report

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 module UsersHelper
 
   def get_progress_report(user,courses)    
-    courses.each do |course|
+    courses.sort.each do |course|
       if course.completed_at.nil?
         last_page_user_completed = course.page_id
         total_pages = course.subject.pages.count 
-        user_progress = last_page_user_completed * 100 / total_pages
+        user_progress = last_page_user_completed / total_pages *100
         @status = "You are #{user_progress} % Completed" 
         @subject = course.subject.name
         return  @subject, @status 
