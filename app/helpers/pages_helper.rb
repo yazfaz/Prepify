d175e2e82f3f388c06203a38d7b0adacf14744fb
veleary <veleary@gmail.com>
2013-10-27 17:47:06 -0500

    logic is working for progress report

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -5,7 +5,8 @@ module PagesHelper
     next_page =  page.subject.pages.find_by_sequence_id(current_page + 1)
     if next_page.nil?
       # How do I grab the user id?
-      
+      course = CourseProgression.find_by(user_id: user.id, subject_id: page.subject.id)
+      course.update!(completed_at: DateTime.new)
       return user_path(@user.id)
     else
       update_course(user, next_page)
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,20 +1,21 @@
 module UsersHelper
 
-  def get_progress_report(user,courses)
-    i = 0
-    while i<= courses.length 
-      courses[i].subject.name
-      if courses[i].completed_at.nil?
-        last_page_user_completed = courses[i].page_id 
-        total_pages = courses[i].subject.pages.count 
+  def get_progress_report(user,courses)    
+    courses.each do |course|
+      if course.completed_at.nil?
+        last_page_user_completed = course.page_id
+        total_pages = course.subject.pages.count 
         user_progress = last_page_user_completed * 100 / total_pages
         @status = "You are #{user_progress} % Completed" 
-        @subject_completed = courses[i].subject.name
-        return  @subject_completed , @status 
+        @subject = course.subject.name
+        return  @subject, @status 
       else
-        status = "Completed!"
-        return courses[i].subject.name , status       
+        @status = "Completed!"
+        @subject = course.subject.name
+        return @subject , @status 
       end
     end 
   end
 end
+
+
