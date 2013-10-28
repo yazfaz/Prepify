02c4a0c9b6e5a8f6eacf0e1f6552a4893c5639ec
veleary <veleary@gmail.com>
2013-10-28 14:52:21 -0500

    update with working progress bars for lesson pages

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,22 +2,17 @@ module UsersHelper
 
   def progress_report(user,courses)    
     report = ""
-
     courses.sort.each do |course|
       if course.completed_at.nil?        
         last_page_user_completed = course.page_id
         total_pages = course.subject.pages.count 
         user_progress = last_page_user_completed * 100 / total_pages
-
         report += content_tag(:h3) { "#{course.subject.name}: You are #{user_progress} % Completed" }
         report += progress_bar(user_progress)
       else
-
-        report += content_tag(:h3, class: 'completed') { "Completed!" }
+        report += content_tag(:h3, class: 'completed') {  "#{course.subject.name}: Completed!" }
       end
-
     end
-
     report.html_safe
   end
 end
