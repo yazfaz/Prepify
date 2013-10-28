bb4c444db3e0b8d9f55c4f06b592409df3a7dd70
veleary <veleary@gmail.com>
2013-10-28 15:43:23 -0500

    update progress report display

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,10 +4,10 @@ module UsersHelper
     report = ""
     courses.sort.each do |course|
       if course.completed_at.nil?        
-        last_page_user_completed = course.page_id
-        total_pages = course.subject.pages.count 
-        user_progress = last_page_user_completed * 100 / total_pages
-        report += content_tag(:h3) { "#{course.subject.name}: You are #{user_progress} % Completed" }
+        last_page_user_completed = course.page_id.to_f
+        total_pages = course.subject.pages.count.to_f 
+        user_progress = (last_page_user_completed / total_pages)*10.0
+        report += content_tag(:h3) { "#{course.subject.name}: You are #{user_progress.round} % Completed" }
         report += progress_bar(user_progress)
       else
         report += content_tag(:h3, class: 'completed') {  "#{course.subject.name}: Completed!" }
