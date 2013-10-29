c376fa59de1d4aac2584f44b42b27397c3a0e0e4
veleary <veleary@gmail.com>
2013-10-28 20:52:38 -0500

    update lesson views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,10 +7,10 @@ module UsersHelper
         last_page_user_completed = course.page_id.to_f
         total_pages = course.subject.pages.count.to_f 
         user_progress = last_page_user_completed / total_pages * 10
-        report += content_tag(:h3) { "#{course.subject.name}: You are #{user_progress.round} % Completed" }
+        report += content_tag(:h3, class: 'panel') { "#{course.subject.name}: You are #{user_progress.round} % Completed " }
         report += progress_bar(user_progress)
       else
-        report += content_tag(:h3, class: 'completed') {  "#{course.subject.name}: Completed!" }
+        report += content_tag(:h3, class: 'panel completed') {  "#{course.subject.name}: Completed!" }
       end
     end
     report.html_safe
