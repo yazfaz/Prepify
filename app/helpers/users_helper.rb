bf325423ac5904d6a898ed495180d75816c6664a
veleary <veleary@gmail.com>
2013-10-29 09:18:31 -0500

    made basic styles for user views for demo

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,8 +4,8 @@ module UsersHelper
     report = ""
     courses.sort.each do |course|
       if course.completed_at.nil?        
-        last_page_user_completed = course.page_id.to_f
-        total_pages = course.subject.pages.count.to_f 
+        last_page_user_completed = course.page_id
+        total_pages = course.subject.pages.count
         user_progress = last_page_user_completed / total_pages * 10
         report += content_tag(:h3, class: 'panel') { "#{course.subject.name}: You are #{user_progress.round} % Completed " }
         report += progress_bar(user_progress)
