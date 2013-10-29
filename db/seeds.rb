b75a132f0dbaa8dbc28c65a6b5b9b40fefe1124c
veleary <veleary@gmail.com>
2013-10-29 17:39:22 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,36 @@
 
 #Create Subjects 
 algebra = Subject.create(section: "Math",name:"Algebra")
+english = Subject.create(section: "English", name: "Reading")
 #Create Pages
 page_1 = algebra.pages.create(sequence_id: 1)
 page_2 = algebra.pages.create(sequence_id: 2)
+page_3 = english.pages.create(sequence_id: 1)
+page_4 = english.pages.create(sequence_id: 2)
+page_5 = english.pages.create(sequence_id: 3)
+page_6 = english.pages.create(sequence_id: 4)
+
 #Create Questions
 ques_1 = Question.create(body: "What is 2 + 2?", choices: [1,3,4,6], correct_answer: 4)
+ques_2 = Question.create(body: "Can you read this?", choices: ["yes", "no"], correct_answer: "yes")
 #Create Instructions
 instruc_1 = Instruction.create(body: "Do this and that!")
+instruc_2 = Instruction.create(body: "Read Letter by Letter")
+instruc_3 = Instruction.create(body: "Read Read Read")
+instruc_4 = Instruction.create(body: "Read Read Read")
 #Link pages to either question/instruction
 page_1.pageable = instruc_1
 page_2.pageable = ques_1
+page_3.pageable = instruc_2
+page_4.pageable = ques_2
+page_5.pageable = instruc_3
+page_6.pageable = instruc_4
 page_1.save!
 page_2.save!
+page_3.save!
+page_4.save!
+page_5.save!
+page_6.save!
 
 #Password/Authentication Data for Devise login
 admin = User.create(
