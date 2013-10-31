cc0c965c795c527d7324a4e17cc9416fec40b35b
Joe Marshall <joseph.charles.marshall@gmail.com>
2013-10-31 12:49:31 -0500

    Expands three meta screens' height

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -2,19 +2,19 @@ $(document).ready(function (){
 	$("#free").click(function() {
 	    $('html, body').animate({
 	        scrollTop: $("#freelanding").offset().top
-	    }, 600);
+	    }, 500);
 	});
 
 	$("#talented").click(function() {
 	    $('html, body').animate({
 	        scrollTop: $("#talentedlanding").offset().top
-	    }, 600);
+	    }, 500);
 	});
 
 	$("#adaptive").click(function() {
 	    $('html, body').animate({
 	        scrollTop: $("#adaptivelanding").offset().top
-	    }, 600);
+	    }, 500);
 	});
 
 });
