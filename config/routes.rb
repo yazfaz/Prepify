b1bf3e083c1a42c9ede9027272b7b8f320b13791
Yasmin Fazelinia <yfazelinia@gmail.com>
2013-10-31 13:49:49 -0500

    delete extra jquery files

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,12 @@ end
 
 # root 'subjects#index'
   root 'admin/subjects#index'
+# devise_scope :user do
+#   root 'devise/sessions#new'
+# end
 
   namespace :admin do
+    root 'admin/subjects#index'
     resources :subjects do
       resources :pages  
         resources :instructions
