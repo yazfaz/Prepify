7dd47d1cc71e9d74b30f107edfcaae272136923b
Yasmin Fazelinia <yfazelinia@gmail.com>
2013-11-04 12:07:12 -0600

    Add AJAX to all sections for publish

--- app/assets/javascripts/admin/subjects.js
+++ app/assets/javascripts/admin/subjects.js
@@ -15,28 +15,10 @@ $(document).on('click', '.show-lesson-form', function (e) {
 $(document).on('click', '.change-published-status', function (e) {
   alert('hi');
   e.preventDefault();
-  // $(e.currentTarget).hide();
   var form = $(e.currentTarget).closest('form');
   console.log('form:', form);
   form.submit();
 });
-// var subjinit = {
-//   publish: function(){
-//     $(document).on('click', '.change-published-status', function(e){
-//       e.preventDefault();
-//     // if ($(e.currentTarget).find('#user_admin').prop('true')){
-//   //   alert('true');
-//       $(e.currentTarget).parent().find('form').submit();
-//       $(e.currentTarget).hide();
 
-//   // }
-//   // else {
-//   //   alert('false');
-//   //   $(e.currentTarget).parent().find('form').submit();
-//   // }
-
-//     });
-//   }
-// }
 
 
