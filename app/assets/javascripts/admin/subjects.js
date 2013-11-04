71f34c1264fc867dc2cf130c5f38e4a429028784
Yasmin Fazelinia <yfazelinia@gmail.com>
2013-11-04 11:54:51 -0600

    Update publish/unpublish using AJAX

--- app/assets/javascripts/admin/subjects.js
+++ app/assets/javascripts/admin/subjects.js
@@ -12,5 +12,31 @@ $(document).on('click', '.show-lesson-form', function (e) {
   form.show();
 });
 
+$(document).on('click', '.change-published-status', function (e) {
+  alert('hi');
+  e.preventDefault();
+  // $(e.currentTarget).hide();
+  var form = $(e.currentTarget).closest('form');
+  console.log('form:', form);
+  form.submit();
+});
+// var subjinit = {
+//   publish: function(){
+//     $(document).on('click', '.change-published-status', function(e){
+//       e.preventDefault();
+//     // if ($(e.currentTarget).find('#user_admin').prop('true')){
+//   //   alert('true');
+//       $(e.currentTarget).parent().find('form').submit();
+//       $(e.currentTarget).hide();
+
+//   // }
+//   // else {
+//   //   alert('false');
+//   //   $(e.currentTarget).parent().find('form').submit();
+//   // }
+
+//     });
+//   }
+// }
 
 
