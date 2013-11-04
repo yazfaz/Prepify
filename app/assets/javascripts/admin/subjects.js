e24b17e05fdaad8cc8c6ea72034d48af8f7804e5
Yasmin Fazelinia <yfazelinia@gmail.com>
2013-11-04 10:25:23 -0600

    All new lessons button hide/show form links working

--- app/assets/javascripts/admin/subjects.js
+++ app/assets/javascripts/admin/subjects.js
@@ -1,32 +1,14 @@
 $(document).on('ajax:success', function(e, data) {
   console.log('Ajax Response data:', data);
   window.e = e;
-  // var subject = $(e.target);
-  // if (e.target.id) == "new_math_lesson") {
-  // $(e.target.id).hide().after('<%= j render partial: 'new_math_form', locals: {subject: @subject} %>');
-  // $('#new_reading_lesson').show();
+
 });
 
 $(document).on('click', '.show-lesson-form', function (e) {
   e.preventDefault();
+  $(e.currentTarget).hide();
   var form = $(e.currentTarget).siblings('form');
   console.log('form:', form);
   form.show();
 });
 
-// var subjectinit = {
-
-//   subjects: function(){
-//     alert('sss');
-//   $('#new_math_lesson').click(function(e){
-//     window.e = e;
-//     $(e.target).hide().after("<%= j render partial: 'new_math_form', locals: {subject: @subject} %>");
-  
-//     // (e.target).parentElement.show("<%= raw render partial: 'new_math_form', locals: {subject: @subject} %>");
-    
-
-//   });
-//   // $('#new_reading_lesson').hide().after('<%= j render partial: 'new_reading_form', locals: {subject: @subject} %>');
-//   // $('#new_writing_lesson').hide().after('<%= j render partial: 'new_writing_form', locals: {subject: @subject} %>');
-//   }
-// }
