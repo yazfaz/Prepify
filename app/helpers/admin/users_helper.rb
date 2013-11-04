cdc7ff88173d4ff59dbe8caff2809780ce21aab1
Yasmin Fazelinia <yfazelinia@gmail.com>
2013-11-04 07:59:38 -0600

    Update admin progress report- remove name and add %

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -7,7 +7,7 @@ module Admin::UsersHelper
         last_page_user_completed = course.page_id
         total_pages = course.subject.pages.count
         user_progress = last_page_user_completed / total_pages * 10
-        report += content_tag(:h3, class: 'report panel incomplete') { "#{course.subject.name}: #{@user.name} is #{user_progress.round} Completed"}
+        report += content_tag(:h3, class: 'report panel incomplete') { "#{course.subject.name}: #{user_progress.round}% Completed"}
         report += progress_bar(user_progress)
       else
         report += content_tag(:h3, class: 'report panel complete') {  "#{course.subject.name}: Completed!" }
